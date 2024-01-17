4b9eaba24ab979e719657e27fa96c60794fb6104
Makuzaza <maria.kuznetsova@edu.bc.fi>
2024-01-17 13:04:12 +0200

    2 week Node Advanced

---
+++ calcProject/calcLibrary.js
@@ -0,0 +1,19 @@
+// calcLibrary.js
+
+'use strict';   
+
+function sum(a,b) {
+
+    if (arguments.length < 2) {
+        throw new Error('parameter missing');
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('only numbers allowed');
+    }
+    return a+b;
+    // if (a && b) {
+    //     return a+b;
+    // }
+}
+
+module.exports = {sum};
--- firstTest/__tests__/demo.test.js
+++ firstTest/__tests__/demo.test.js
@@ -10,7 +10,7 @@ afterAll(()=>{
 
 test('this is the first test', ()=>{
     console.log('test one')
-    expect('1'+'2').tobe('12')
+    expect('1'+'2').toBe('12')
 })
 
 it('this is the second test', ()=>{
@@ -40,13 +40,13 @@ describe('Group B', ()=>{
     describe('group B.A', ()=>{
         test('test 1 in B.A', ()=>{
             console.log('test 1 in group B.A');
-            expect('1'+'2').tobe('12')
+            expect('1'+'2').toBe('12')
         })
     })
     describe('group B.B', ()=>{
         test('test 1 in B.B', ()=>{
             console.log('test 1 in group B.B');
-            expect('1'+'2').tobe('12')
+            expect('1'+'2').toBe('12')
         })
     })
 })
